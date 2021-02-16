577b67f762d833988024aea3c9681481028b6057
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:36:44 -0300

    Add files via upload

---
+++ Uri1071/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uri1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, maior, menor, impar;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            impar = 0;
+            if(x>y)
+            {
+                maior = x;
+                menor = y;
+            }
+            else
+            {
+                maior = y;
+                menor = x;
+            }
+
+            for(int c=menor+1;c<maior;c++)
+            {
+
+                if(c%2!=0)
+                {
+                    impar =impar+c;
+                }
+
+            }
+
+            Console.WriteLine(impar);
+
+        }
+    }
+}
